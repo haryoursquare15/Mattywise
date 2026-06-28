be3bf1a720a04725d17d1e043c028bc7876f5db6
haryoursquare15 <108743600+haryoursquare15@users.noreply.github.com>
2026-06-28 19:09:36 +0100

    Update storage.ts

--- artifacts/api-server/src/routes/storage.ts
+++ artifacts/api-server/src/routes/storage.ts
@@ -41,6 +41,9 @@ router.post(
       const uploadURL = await objectStorageService.getObjectEntityUploadURL();
       const objectPath = objectStorageService.normalizeObjectEntityPath(uploadURL);
 
+      console.log("UPLOAD URL:", uploadURL);
+      console.log("OBJECT PATH:", objectPath);
+
       res.json(
         RequestUploadUrlResponse.parse({
           uploadURL,
